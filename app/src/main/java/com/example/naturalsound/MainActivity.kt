f9a17582281a44f45e42fbaf941f48fdbdfba150
Xakimov <dxakimov@cdti.uz>
2025-01-14 17:29:11 +0500

    minor change

--- app/src/main/java/com/example/naturalsound/MainActivity.kt
+++ app/src/main/java/com/example/naturalsound/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : ComponentActivity() {
     private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             ActivityCompat.requestPermissions(
                 this,
