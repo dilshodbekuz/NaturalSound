a95d8f1044e1debbbae1121ae6b02c33787569c7
Mirjamol <mirjamolq828@gmail.com>
2025-01-16 11:45:32 +0300

    wind music and images added

--- app/src/main/java/com/example/naturalsound/main/MainViewModel.kt
+++ app/src/main/java/com/example/naturalsound/main/MainViewModel.kt
@@ -29,32 +29,36 @@ class MainViewModel : ViewModel() {
 
 
     private val sounds = persistentListOf(
-        SoundModel(1, "Rain", R.raw.rain, R.drawable.rain),
-        SoundModel(2, "Fire", R.raw.fire, R.drawable.fire),
-        SoundModel(3, "Typing", R.raw.typing, R.drawable.typing),
-        SoundModel(4, "Birds", R.raw.birds, R.drawable.birds),
-        SoundModel(5, "Ocean", R.raw.ocean, R.drawable.ocean),
-        SoundModel(6, "Water Full", R.raw.water_full, R.drawable.waterfall),
-        SoundModel(7, "Drip", R.raw.drip, R.drawable.drip),
-        SoundModel(8, "Storm", R.raw.storm, R.drawable.storm),
-        SoundModel(9, "Wind", R.raw.wind, R.drawable.strong_wind),
-        SoundModel(10, "Bird", R.raw.bird_pure_sound, R.drawable.pure_bird),
-        SoundModel(11, "Birds and Frogs", R.raw.birds_vs_frog, R.drawable.frog_and_bords),
-        SoundModel(12, "Calm Rain", R.raw.calm_rain, R.drawable.calm_rain),
-        SoundModel(13, "Calm River", R.raw.calm_river, R.drawable.calm_river),
-        SoundModel(14, "Storm and Rain", R.raw.storm_and_rain, R.drawable.storm_rain),
-        SoundModel(15, "Strong Wind", R.raw.strong_wild_wind, R.drawable.strong_wind),
-        SoundModel(16, "Mountain River", R.raw.calm_mountain_river, R.drawable.mountain_river),
-        SoundModel(17, "Calm Forest", R.raw.calm_forest, R.drawable.calm_forest),
-        SoundModel(18, "Jungle", R.raw.jungle, R.drawable.jungle),
-        SoundModel(19, "Orphean", R.raw.eastern_orphean_bird, R.drawable.eastern_orphean),
-        SoundModel(20, "Erithacus Rebecula", R.raw.erithacus_rebecula_bird, R.drawable.robin_bird),
-        SoundModel(21, "Greater Hoopoe", R.raw.greater_hoopoe_bird, R.drawable.greater_hoopoe),
-        SoundModel(22, "Hermit Thrush", R.raw.hermit_thrush_bird, R.drawable.hermit_thrush_bird),
-        SoundModel(23, "Indian Malabar", R.raw.indian_malabar_bird, R.drawable.indian_malabar_bird),
-        SoundModel(25, "Olive Whistler", R.raw.olive_whistler_bird, R.drawable.olive_whistler),
-        SoundModel(26, "Pied Butcherbird", R.raw.pied_butcherbird, R.drawable.pied_butcherbird),
-        SoundModel(27, "Woodlark", R.raw.woodlark_bird, R.drawable.woodlark_bird),
+        SoundModel(1, "Typing", R.raw.typing, R.drawable.typing),
+        SoundModel(2, "Rain", R.raw.rain, R.drawable.rain),
+        SoundModel(3, "Calm Rain", R.raw.calm_rain, R.drawable.calm_rain),
+        SoundModel(4, "Storm and Rain", R.raw.storm_and_rain, R.drawable.storm_rain),
+        SoundModel(5, "Water Full", R.raw.water_full, R.drawable.waterfall),
+        SoundModel(6, "Calm River", R.raw.calm_river, R.drawable.calm_river),
+        SoundModel(7, "Mountain River", R.raw.calm_mountain_river, R.drawable.mountain_river),
+        SoundModel(8, "Ocean", R.raw.ocean, R.drawable.ocean),
+        SoundModel(9, "Drip", R.raw.drip, R.drawable.drip),
+        SoundModel(10, "Fire", R.raw.fire, R.drawable.fire),
+        SoundModel(11, "Storm", R.raw.storm, R.drawable.storm),
+        SoundModel(12, "Thunder", R.raw.long_thunder, R.drawable.long_thunder),
+        SoundModel(13, "Peals of Thunder", R.raw.peals_of_thunder, R.drawable.peals_of_thunder),
+        SoundModel(14, "Rain and Thunder", R.raw.rain_mix_thunder, R.drawable.rain_and_thunder),
+        SoundModel(15, "Rain Thunder Wind", R.raw.thunder_rain_wind_mixed, R.drawable.rain_thunder_wind_mixed),
+        SoundModel(16, "Wind", R.raw.wind, R.drawable.normal_wind),
+        SoundModel(17, "Strong Wind", R.raw.strong_wild_wind, R.drawable.strong_wind),
+        SoundModel(18, "Calm Forest", R.raw.calm_forest, R.drawable.calm_forest),
+        SoundModel(19, "Jungle", R.raw.jungle, R.drawable.jungle),
+        SoundModel(20, "Orphean", R.raw.eastern_orphean_bird, R.drawable.eastern_orphean),
+        SoundModel(21, "Erithacus Rebecula", R.raw.erithacus_rebecula_bird, R.drawable.robin_bird),
+        SoundModel(22, "Bird", R.raw.bird_pure_sound, R.drawable.pure_bird),
+        SoundModel(23, "Greater Hoopoe", R.raw.greater_hoopoe_bird, R.drawable.greater_hoopoe),
+        SoundModel(24, "Hermit Thrush", R.raw.hermit_thrush_bird, R.drawable.hermit_thrush_bird),
+        SoundModel(25, "Indian Malabar", R.raw.indian_malabar_bird, R.drawable.indian_malabar_bird),
+        SoundModel(26, "Birds and Frogs", R.raw.birds_vs_frog, R.drawable.frog_and_bords),
+        SoundModel(27, "Olive Whistler", R.raw.olive_whistler_bird, R.drawable.olive_whistler),
+        SoundModel(28, "Pied Butcherbird", R.raw.pied_butcherbird, R.drawable.pied_butcherbird),
+        SoundModel(29, "Woodlark", R.raw.woodlark_bird, R.drawable.woodlark_bird),
+        SoundModel(30, "Birds", R.raw.birds, R.drawable.birds),
     )
 
     init {
@@ -67,7 +71,7 @@ class MainViewModel : ViewModel() {
             selectList.addAll(uiState.value.selectList)
             selectList.add(item)
             _uiState.update { it.copy(selectList = selectList.toImmutableList()) }
-            val mediaPlayer = MediaPlayer.create(context, item.sound ).apply {
+            val mediaPlayer = MediaPlayer.create(context, item.sound).apply {
                 isLooping = true
                 start()
             }
